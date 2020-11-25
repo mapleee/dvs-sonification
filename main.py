fd14a0f49d5b1bdfb8a5c5919da5a57f2bd57ec7
mapleee <839354647@qq.com>
2020-11-25 15:38:51 +0800

    suitable for new event txt

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import numpy as np
 import glob
-import cv2
 import os
 import tqdm
 from utils import event2music
-from utils.config import *
 
 
 event_dir = 'data/event'
@@ -25,27 +22,16 @@ if __name__ == '__main__':
         event_type = os.path.splitext(os.path.basename(event_txt))[0]
         events_coo_list, coo_accu_list = event2music.process_events(event_txt)
 
-        # timestamps = np.unique(events[:, 0])
+        # 写midi
+        midi_path = f'{music_dir}/{event_type}.mid'
+        event2music.pitch2midi(events_coo_list, midi_path)
 
-        # # 写图片
-        # os.makedirs(f'{pic_dir}/{event_type}', exist_ok=True)
-        # for i, time in enumerate(timestamps):
-        #     pic_fn = f'{pic_dir}/{event_type}/pic_{i}.png'
-        #     event2music.write_event_pic(time, events, pic_fn)
-        # break
+        # 写audio
+        audio_path = f'{music_dir}/{event_type}.wav'
+        event2music.midi2audio(sf2_path, midi_path, audio_path)
 
-        # # 写midi
-        # midi_path = f'{music_dir}/{event_type}.mid'
-        # event2music.pitch2midi(timestamps, events, midi_path)
-        #
-        # # 写audio
-        # audio_path = f'{music_dir}/{event_type}.wav'
-        # event2music.midi2audio(sf2_path, midi_path, audio_path)
-        #
         # 写视频
         video_path = f'{video_dir}/{event_type}.avi'
-        video_sound_path = f'{video_dir}/{event_type}_with_sound.avi'
-        event2music.write_event_video(coo_accu_list, video_path)
-
-        # event2music.write_event_video(events, timestamps, video_path, audio_path, video_sound_path)
+        video_audio_path = f'{video_dir}/{event_type}_with_sound.avi'
+        event2music.write_event_video(coo_accu_list, video_path, audio_path, video_audio_path)
 
--- utils/event2music.py
+++ utils/event2music.py
@@ -8,16 +8,7 @@ from collections import Counter
 from utils.config import *
 
 
-def write_event_pic(time, events, pic_fn):
-    img = np.zeros((HEIGHT, WIDTH), np.uint8)
-    events_coo = get_event_coordinate(time, events)
-    for j, coo in enumerate(events_coo):
-        img[int(coo[1]), int(coo[0])] = 255
-    # img = cv2.medianBlur(img, 3)
-    cv2.imwrite(pic_fn, img)
-
-
-def write_event_video(coo_accu_list, video_path):
+def write_event_video(coo_accu_list, video_path, audio_path, video_audio_path):
     fourcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
     out = cv2.VideoWriter(video_path, fourcc, FPS, (WIDTH, HEIGHT))
     for coo_accu in coo_accu_list:
@@ -30,23 +21,16 @@ def write_event_video(coo_accu_list, video_path):
         img_3c = cv2.merge([img, img, img])
         out.write(img_3c)
     out.release()
-    # os.system(f'ffmpeg -i {video_fn} -i {audio_fn} -c copy {video_sound_fn}')
-
-
-def get_event_coordinate(time, events):
-    '''
-
-    :param time: The timestamp of events
-    :param events: The event flow read from txt
-    :return: The events of the timestamp, using list format, shape:[len(events), 2]
-    '''
-
-    idx = np.where(events[:, 0] == time)
-    event_coo = events[idx, 1:3].reshape(-1, 2)
-    return event_coo
+    os.system(f'ffmpeg -i {video_path} -i {audio_path} -c copy {video_audio_path}')
 
 
 def process_events(event_txt):
+    '''
+
+    :param event_txt:
+    :return: events_coo_list: 每帧图像的事件列表，列表中每个元素为一个二维（dim:[num_event, 2]）数组
+             coo_accu_list: 每帧图像的事件列表，附加一个事件出现的次数，列表中每个元素为一个二维（dim:[num_event, 3]）数组
+    '''
     # 获取每帧图像中事件的坐标
     events = np.genfromtxt(event_txt)
     events[:, 0] = (events[:, 0] - events[0, 0]) // PACKET_TIME
@@ -72,21 +56,21 @@ def process_events(event_txt):
     return events_coo_list, coo_accu_list
 
 
-def pitch_played_for_frame(event_coo, iii):
+def pitch_played_for_frame(start_time, events_coo):
     '''
-    对于一个timestamp，其中事件个数在指定高度范围内的数量大于设定阈值，则该区域存在音符
-    :param pitch_list:
-    :param event_coo:
+    对于一帧图像，其中事件个数在指定高度范围内的数量大于设定阈值，则该区域存在音符
+    :param start_time:
+    :param events_coo:
     :return:
     '''
 
-    e_pitch = event_coo[:, 1] // (HEIGHT // (OCTAVE_NUM*12))  # 表示一个事件高度对应哪一个音高
+    e_pitch = events_coo[:, 1] // (HEIGHT // (OCTAVE_NUM*12))  # 表示一个事件高度对应哪一个音高
     pitch_counter = Counter(e_pitch)
 
     # 去噪，如果总事件数低于设定值，则证明该帧图像为噪声
     event_num = sum(pitch_counter.values())
     if event_num < TOTAL_EVENT_THRESHOLD:
-        return None
+        return None, None
 
     # 得到所有符合条件（事件数大于阈值）的音高序列
     pitch_played_index = []
@@ -97,48 +81,64 @@ def pitch_played_for_frame(event_coo, iii):
     pitch_played_index = np.sort((OCTAVE_NUM*12) - np.array(pitch_played_index))
 
     if len(pitch_played_index) == 0:
-        return None
+        return None, None
+
     # 求出最高音与最低音的差，查表获得使用音符
     pitch_diff = pitch_played_index[-1] - pitch_played_index[0]
     if SCALE_TYPE == 'Major':
         pitch_played = np.array(PITCH_MAJOR[int(pitch_diff)]) + ROOT_PITCH + int(pitch_played_index[0])
+        notes_list = []
+        for pitch in pitch_played:
+            note = pretty_midi.Note(velocity=100, pitch=pitch, start=start_time/1e6, end=(start_time+PACKET_TIME)/1e6)
+            notes_list.append(note)
+        # 加上control change事件
+        cc_value = int(np.mean(events_coo[:, 0]) * 127 / WIDTH)
+        cc = pretty_midi.ControlChange(number=10, value=cc_value, time=start_time/1e6)
     else:
         raise NotImplementedError
 
-    return pitch_played
+    return notes_list, cc
 
 
-def pitch2midi(timestamps, events, midi_path):
-    pitch_time = (timestamps - np.min(timestamps)) / 1e6  # 把第一个时间步设为0，把单位统一为秒
+def pitch2midi(events_coo_list, midi_path):
     midi_data = pretty_midi.PrettyMIDI()
     piano_all = pretty_midi.Instrument(program=0)
-    piano_concat = pretty_midi.Instrument(program=0)
-    events_coo_total = []
+    # piano_concat = pretty_midi.Instrument(program=0)
 
-    for i, time in enumerate(timestamps):
-        # 舍弃最后几个timestamp
-        if (len(timestamps) - i) <= (len(timestamps) % PACKET_NUM):
-            break
-        events_coo = get_event_coordinate(time, events)
-        events_coo_total.append(events_coo)
-        if (i + 1) % PACKET_NUM != 0:
+    for i, events_coo in enumerate(events_coo_list):
+        notes_list, cc = pitch_played_for_frame(i*PACKET_TIME, events_coo)
+        if (notes_list is None) and (cc is None):
             continue
-        else:
-            events_coo_total = np.concatenate(events_coo_total)
-            pitch_played = pitch_played_for_frame(events_coo_total, i)
-            if pitch_played is None:
-                events_coo_total = []
-                continue
-            cc_value = int(np.mean(events_coo_total[:, 0])*127/WIDTH)
-            cc = pretty_midi.ControlChange(number=10, value=cc_value, time=pitch_time[i])
-            piano_all.control_changes.append(cc)
-            piano_concat.control_changes.append(cc)
-            for pitch in pitch_played:
-                note = pretty_midi.Note(velocity=100, pitch=pitch, start=pitch_time[i], end=pitch_time[i+PACKET_NUM])
-                piano_all.notes.append(note)
-            events_coo_total = []
+        piano_all.notes.extend(notes_list)
+        piano_all.control_changes.append(cc)
 
+    # midi_data.instruments.append(piano_concat)
+    midi_data.instruments.append(piano_all)
+    midi_data.write(midi_path)
+
+    return midi_data
+
+
+def midi2audio(sf2_path, midi_path, audio_path):
+    sf2 = FluidSynth(sf2_path)
+    sf2.midi_to_audio(midi_path, audio_path)
+
+
+def numpy_multi_sort(array):
+    value = [tuple(a) for a in array]
+    dtyp = [('x', int), ('y', int)]
+    array = np.array(value, dtype=dtyp)
+    sorted_arr = np.sort(array, order=['x', 'y'])
+    sorted_arr = np.array([list(a) for a in sorted_arr])
+    return sorted_arr
+
+
+def pitch_concat(midi_data):
     # 音高拼接，把所有破碎的音符拼接起来
+    new_midi_data = pretty_midi.PrettyMIDI()
+    piano_concat = pretty_midi.Instrument(program=0)
+
+    piano_all = midi_data.instruments[0]
     piano_all.notes = sorted(piano_all.notes, key=lambda x: (x.pitch, x.start))  # 对所有音符按照音高和开始时间排序
     flag = 0
     for i, note in enumerate(piano_all.notes):
@@ -160,26 +160,8 @@ def pitch2midi(timestamps, events, midi_path):
                 new_note = pretty_midi.Note(velocity=100, pitch=pitch, start=start, end=end)
                 piano_concat.notes.append(new_note)
                 flag = 0
-
-    midi_data.instruments.append(piano_concat)
-    # midi_data.instruments.append(piano_all)
-    midi_data.write(midi_path)
-
-    return midi_data
-
-
-def midi2audio(sf2_path, midi_path, audio_path):
-    sf2 = FluidSynth(sf2_path)
-    sf2.midi_to_audio(midi_path, audio_path)
-
-
-def numpy_multi_sort(array):
-    value = [tuple(a) for a in array]
-    dtyp = [('x', int), ('y', int)]
-    array = np.array(value, dtype=dtyp)
-    sorted_arr = np.sort(array, order=['x', 'y'])
-    sorted_arr = np.array([list(a) for a in sorted_arr])
-    return sorted_arr
+    new_midi_data.instruments.append(piano_concat)
+    return new_midi_data
 
 
 if __name__ == '__main__':
